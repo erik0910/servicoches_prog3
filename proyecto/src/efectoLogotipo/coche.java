34d8a168e926b6986907dee7fc46428d706311a3
admin <admin@anonimo>
2018-11-23 19:13:16 +0100

    efecto logotipo

---
+++ proyecto/src/efectoLogotipo/coche.java
@@ -0,0 +1,56 @@
+package efectoLogotipo;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+public class coche extends JFrame {
+	//se ha creado esta clase para probar y mejorra el logotipo del proyecto
+	//todavia no es un efecto final y es por eso que no se ha implementado en la aplicacion
+	Graphics g;
+	BufferedImage imagen=null;
+	String nombre="coche";
+	int i=1;
+	int count=0;
+	public coche() {
+		getContentPane().setBackground(Color.LIGHT_GRAY);
+		getContentPane().setForeground(Color.LIGHT_GRAY);
+		pack();
+		setLocationRelativeTo(null);
+		setSize(new Dimension(1100, 1100));
+		setPreferredSize(getSize());
+		setVisible(true);
+		paint(g);
+		
+	}
+	public static void main(String[] args) {
+		new coche();
+	}
+
+	@Override
+	public void paint(Graphics g1) {
+		try {
+			imagen=ImageIO.read(getClass().getResource("/imagenes/"+nombre+i+".jpg"));
+			g1.drawImage(imagen, 30, 60, 900,200, null);
+			//el objetivo de la funcion count es unicamente la de repetir imagenes para hacer la transiciones mas
+			//lentas
+			if(i<10 && count==9) {i++;count=0; }
+			else if(count<9){count++;}
+			else {/*dispose();*/}//para que el hilo no sigua corriendo y gastando recursos
+			repaint();
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			
+			e.printStackTrace();
+		}
+		
+	}
+}
--- proyecto/src/interfaz/Registro.java
+++ proyecto/src/interfaz/Registro.java
@@ -1,22 +1,35 @@
 package interfaz;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JTextField;
-import javax.swing.JLabel;
-import javax.swing.JButton;
 
-public class Registro extends JFrame {
+import Objetos.USUARIO;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import javax.swing.JPasswordField;
+
+public class Registro extends JFrame implements ActionListener{
 	private JTextField textField;
-	private JTextField textField_1;
+	ArrayList<USUARIO>p1;
+	private JPasswordField passwordField;
 	public Registro() {
 		setSize(new Dimension(900, 900));
 		setPreferredSize(getSize());
 		pack(); 
 		getContentPane().setLayout(null);
+		p1=new ArrayList<>();
 		
 		textField = new JTextField();
 		textField.setBounds(285, 203, 373, 45);
@@ -31,22 +44,20 @@ public class Registro extends JFrame {
 		lblContrasea.setBounds(51, 382, 188, 39);
 		getContentPane().add(lblContrasea);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(285, 376, 373, 45);
-		getContentPane().add(textField_1);
-		textField_1.setColumns(10);
-		
 		JButton btnRegistrar = new JButton("Registrar");
 		btnRegistrar.setBounds(272, 552, 197, 47);
 		getContentPane().add(btnRegistrar);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(285, 379, 373, 42);
+		getContentPane().add(passwordField);
 		setVisible(true);
 	
 	}
 	public static void main(String[] args) {
-		new Registro()
-		;
+		new Registro();
 	}
-	public static void meterdatos() {
+	public static void meterdatos(USUARIO usuario) {
 		try {
 			FileWriter e=new FileWriter("usuarios.csv");
 			
@@ -56,5 +67,46 @@ public class Registro extends JFrame {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	public void leerFichero() {
+		try {
+			FileReader a=new FileReader("usuarios.csv");
+			BufferedReader b=new BufferedReader(a);
+			String linea=null;
+			while((linea=b.readLine())!=null) {
+				int i=0;
+				System.out.println(linea);
+				String ac[]=linea.split(";");
+			USUARIO p=new USUARIO(ac[0], ac[1], Integer.parseInt(ac[2]));
+			p1.add(p);}
+			
+			b.close();
+			a.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		}
+	public boolean compararContra() {
+		for (USUARIO usuario : p1) {
+			if(usuario.equals(textField.getText())) {return false;}
+		}
+		return true;
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(compararContra()) {
+			USUARIO usuario=new USUARIO(textField.getText(), passwordField.getText(), 0);
+			p1.add(usuario);
+		meterdatos(usuario);
+		new  InicioSesion();
+		this.dispose();
+		}
+		JOptionPane.showConfirmDialog(null, "el usuario ya esta por favor cambie de nombre");
+		
 	}
 }
